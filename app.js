3009e9aa12016b4184f68704f036ed404ce4ed2c
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2023-12-30 11:59:09 -0300

    Hello World e primeiro codigo executado com javascript e nodejs

---
+++ app.js
@@ -0,0 +1,9 @@
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+console.log("Hello World!");
+console.log("Meu nome e Luis");
+console.log("E eu estou aprendendo nodejs com o guia do programador");
+
+if(mostrarSite){ //se mostar o site for "true"
+    console.log(site); //printa o site
+}
